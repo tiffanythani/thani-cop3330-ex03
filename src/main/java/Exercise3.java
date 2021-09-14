579eeaf1e99d106fbd65f920bc4e8c725f8f3656
Tiffany Thani <tiffanythani18@gmail.com>
2021-09-13 23:52:15 -0400

    Initial commit

---
+++ src/main/java/Exercise3.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Tiffany Thani
+ */
+
+
+
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+
+
+
+
+        {
+            String input;
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter a quote");
+            input = scan.next();
+        }
+            System.out.print("Enter the author of the quote");
+
+            String A = scan.next();
+
+
+            System.out.print("" + input"" + "\n" + ah)
+
+
+    }
+}
